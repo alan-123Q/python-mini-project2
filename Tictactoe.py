f057e441be497c6ed5c6ce077fa6ce0fa6e49872
ALAN S VARGHESE <alansvarghese7@gmail.com>
2025-06-09 18:26:52 +0530

    Add files via upload

---
+++ Tictactoe.py
@@ -0,0 +1,46 @@
+import os
+from spot import check_turn, check_for_win, draw_board
+
+# Declare all variables 
+spots = {1 : '1', 2 : '2', 3: '3', 4 : '4', 5 : '5', 
+         6 : '6', 7 : '7',  8 : '8', 9 : '9'}
+playing, complete = True, False
+turn = 0
+prev_turn = -1
+while playing:
+    # Reset the screen
+    os.system('cls' if os.name == 'nt' else 'clear')
+    # Draw the current Game Board
+    draw_board(spots)
+    if prev_turn == turn:
+      print("Invalid spot selected, please pick another.")
+    prev_turn = turn
+    print("Player " + str((turn % 2) +1 ) + "'s turn: Pick your spot or press q to quit")
+    
+    
+    choice = input()
+    # The game has ended, 
+    if choice == 'q':
+        playing = False
+    elif str.isdigit(choice) and int(choice) in spots:
+      # Check if the spot is already taken.
+      if not spots[int(choice)] in {"X", "O"}:
+        # If not, update board and increment the turn
+        turn += 1
+        spots[int(choice)] = check_turn(turn)
+      
+    # Check if the game has ended (and if someone won)
+    if check_for_win(spots): playing, complete = False, True
+    if turn > 8: playing = False
+    
+# Update the board one last time. 
+os.system('cls' if os.name == 'nt' else 'clear')
+draw_board(spots)
+if complete:
+  if check_turn(turn) == 'X': print("Player 1 Wins!")
+  else: print("Player 2 Wins!")
+else: 
+  # Tie Game
+  print("No Winner")
+  
+print("Thanks for playing!") 
